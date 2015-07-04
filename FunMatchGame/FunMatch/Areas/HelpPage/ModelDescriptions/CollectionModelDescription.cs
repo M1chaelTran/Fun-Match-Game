f7229ab4e5f75ac57d39ca76b271d6d4bab1325a
triqi <tri.quang.tran@gmail.com>
2015-07-04 13:13:42 +1000

    initial commit.

---
+++ FunMatchGame/FunMatch/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace FunMatch.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ FunMatchGame/FunMatch/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FunMatch.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
