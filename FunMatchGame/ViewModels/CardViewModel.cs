f233c9b481b14e923eb33d18767ecd4ff5a781cb
Michael Tran <michael.tran@ansarada.com>
2015-07-04 14:24:52 +1000

    Add CardViewModel

---
+++ FunMatchGame/ViewModels/CardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FunMatchGame.ViewModels
+{
+	public class CardViewModel
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string ImageUrl { get; set; }
+		public string SourceUrl { get; set; }
+	}
+}
