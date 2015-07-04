9a66437c4734a489f6490806e2e34b3dd3050a76
tri <tri.quang.tran@gmail.com>
2015-07-04 14:12:34 +1000

    angular bootstrap.

--- FunMatchGame/App_Start/BundleConfig.cs
+++ FunMatchGame/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace FunMatchGame
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
+                        "~/Scripts/angularjs.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
